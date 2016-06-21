c8e2477ef1bd6aa660fb53bba1215734386afbcc
Andrew Hao <andrew@carbonfive.com>
2016-06-21 13:47:12 -0700

    Remove extra logging

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,7 +4,6 @@ defmodule VelocitasIdentity.PageController do
   def index(conn, _params) do
     token = Guardian.Plug.current_token(conn)
     user = Guardian.Plug.current_resource(conn)
-    Apex.ap(user)
     render conn, "index.html", current_user: user
   end
 end
