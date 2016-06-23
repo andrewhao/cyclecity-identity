926c9c6a291a9825ee022bc42586da69dbedd772
Andrew Hao <andrewhao@gmail.com>
2016-06-23 14:45:33 -0700

    Integrate Rails session storage plug, for cross-Rails app compat. (#5)

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,8 +1,16 @@
 defmodule VelocitasIdentity.PageController do
   use VelocitasIdentity.Web, :controller
+  use Timex
 
   def index(conn, _params) do
-    token = Guardian.Plug.current_token(conn)
+
+    {:ok, date} = Timex.format(DateTime.now, "{ISO:Extended}")
+    Apex.ap get_session(conn, :web_message)
+    Apex.ap get_session(conn, :session_id)
+    Apex.ap get_session(conn, :api_message)
+    Apex.ap get_session(conn, :current_user)
+    conn = put_session(conn, :web_message, "Web received: #{date}")
+
     user = Guardian.Plug.current_resource(conn)
     render conn, "index.html", current_user: user
   end
