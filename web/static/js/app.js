249478b5c5b1f87ed2cd8887d0dc335731bb26e1
Andrew Hao <andrewhao@gmail.com>
2016-06-28 17:14:07 -0700

    First pass react.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,12 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+import React                    from 'react';
+import ReactDOM                 from 'react-dom';
+import { browserHistory }       from 'react-router';
+import configureStore           from './store';
+import Root                     from './containers/root';
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
+const store  = configureStore(browserHistory);
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+const target = document.getElementById('main_container');
+const node = <Root routerHistory={browserHistory} store={store}/>;
 
-// import socket from "./socket"
+ReactDOM.render(node, target);
