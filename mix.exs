d8bab4a7345bda533cc6ad28e3bc8766c661f37a
Andrew Hao <andrew@carbonfive.com>
2016-12-29 22:16:44 -0800

    Specifying Erlang 19 and Elixir 1.3.4, then upgrading libs to match

--- mix.exs
+++ mix.exs
@@ -39,10 +39,10 @@ defmodule VelocitasIdentity.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.4"},
+    [{:phoenix, "~> 1.2"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 2.0"},
-     {:phoenix_html, "~> 2.4"},
+     {:phoenix_ecto, "~> 2.0.1"},
+     {:phoenix_html, "~> 2.6.0"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:mix_test_watch, "~> 0.2", only: :dev},
      {:gettext, "~> 0.9"},
